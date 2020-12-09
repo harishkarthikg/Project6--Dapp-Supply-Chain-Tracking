b4c0f577f17c7a20d26b9e0c3190ab1dfc1bf652
Harishkarthik <gunalan2@illinois.edu>
2020-12-08 23:00:08 -0500

    Add files via upload

---
+++ truffle-config.js
@@ -0,0 +1,56 @@
+const HDWalletProvider  = require('@truffle/hdwallet-provider');
+const infuraKey = "1c00a0de84704892bc5a99a557ad987d";
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync("./secret.txt").toString().trim();
+
+module.exports = {
+
+
+  networks: {
+
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 9545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+    },
+
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+      network_id: 4,       // rinkeby's id
+      gas: 4500000,        // rinkeby has a lower block limit than mainnet
+      gasPrice: 21000000000,
+      networkCheckTimeout: 1000000
+    },
+
+
+     ropsten: {
+      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${infuraKey}`),
+      network_id: 3,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      networkCheckTimeout: 1000000000,
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
+
+
+  mocha: {
+     timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "^0.4.23",    // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+      //  optimizer: {
+      //    enabled: false,
+      //    runs: 200
+      //  },
+      //  evmVersion: "byzantium"
+      // }
+    }
+  }
+}
